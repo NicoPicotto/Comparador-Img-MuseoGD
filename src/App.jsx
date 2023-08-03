7d917f155c7e0b4d484a25bdea25d0ca29d9c1e1
NicoPicotto <nicopicotto@hotmail.com>
2023-08-03 14:15:19 -0300

    new layout

--- src/App.jsx
+++ src/App.jsx
@@ -6,11 +6,23 @@ import img1 from './assets/15-8.jpg';
 import img1c from './assets/15-8c.jpg';
 import img2 from './assets/1913.jpg';
 import img2c from './assets/1913c.jpg';
+import img3 from './assets/Agr.jpg';
+import img3c from './assets/Agrc.jpg';
+import img4 from './assets/DFC28.jpg';
+import img4c from './assets/DFC28c.jpg';
+import img5 from './assets/ESC.jpg';
+import img5c from './assets/ESCc.jpg';
+import img6 from './assets/Fog.jpg';
+import img6c from './assets/Fogc.jpg';
 
 function App() {
 	const images = [
 		{ id: 1, bw: img1, color: img1c },
 		{ id: 2, bw: img2, color: img2c },
+		{ id: 3, bw: img3, color: img3c },
+		{ id: 4, bw: img4, color: img4c },
+		{ id: 5, bw: img5, color: img5c },
+		{ id: 6, bw: img6, color: img6c },
 		// add more images here
 	];
 
@@ -38,7 +50,7 @@ function App() {
 					}
 				/>
 
-				<Route path='/compare/:id' element={<CompareView />} />
+				<Route path='/compare/:id' element={<CompareView images={images} />} />
 			</Routes>
 		</Router>
 	);
--- src/Views/CompareView.jsx
+++ src/Views/CompareView.jsx
@@ -1,25 +1,19 @@
 import React from 'react';
-import { useParams } from 'react-router-dom';
+import { useParams, Link } from 'react-router-dom';
 import './imageCompare.css';
 import ReactCompareImage from 'react-compare-image';
 import Spinner from '../Spinner/Spinner';
-import img1 from '../assets/15-8.jpg';
-import img1c from '../assets/15-8c.jpg';
-import img2 from '../assets/1913.jpg';
-import img2c from '../assets/1913c.jpg';
+import back from '../assets/back.png';
 
-function CompareView() {
-	const images = [
-		{ id: 1, bw: img1, color: img1c },
-		{ id: 2, bw: img2, color: img2c },
-		// add more images here
-	];
-
+function CompareView({ images }) {
 	const { id } = useParams();
 	const image = images.find((image) => image.id === Number(id));
 
 	return (
 		<div className='imgCompare'>
+			<Link to='/' class='imgBack'>
+				<img src={back} className='arrow' />
+			</Link>
 			<ReactCompareImage
 				leftImage={image.bw}
 				rightImage={image.color}
